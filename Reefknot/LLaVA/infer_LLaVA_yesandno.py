273f7f9c9ee5f93fd49c59f5665b5d1b612fc87b
manuthomas99 <manuthomas233@gmail.com>
2025-11-11 19:31:52 +0100

    max_new_token as param

--- Reefknot/LLaVA/infer_LLaVA_yesandno.py
+++ Reefknot/LLaVA/infer_LLaVA_yesandno.py
@@ -155,7 +155,7 @@ def eval_model(args):
                     temperature=args.temperature,
                     top_p=args.top_p,
                     num_beams=args.num_beams,
-                    max_new_tokens=10,
+                    max_new_tokens=args.max_new_tokens,
                     use_cache=True,
                     output_scores=True,
                     apha=args.apha,
@@ -172,7 +172,7 @@ def eval_model(args):
                 temperature=args.temperature,
                 top_p=args.top_p,
                 num_beams=args.num_beams,
-                max_new_tokens=10,
+                max_new_tokens=args.max_new_tokens,
                 use_cache=True,
                 output_scores=True
             )
@@ -210,6 +210,7 @@ if __name__ == "__main__":
     parser.add_argument("--temperature", type=float, default=0.2)
     parser.add_argument("--top_p", type=float, default=None)
     parser.add_argument("--num_beams", type=int, default=1)
+    parser.add_argument("--max_new_tokens", type=int, default=2)
     parser.add_argument("--model_type", type=str, default=None)
     parser.add_argument("--apha", type=float, default=0.1)
     parser.add_argument("--threshold", type=float, default=0.9)
