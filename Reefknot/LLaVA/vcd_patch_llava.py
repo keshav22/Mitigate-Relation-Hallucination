9df5c814b9a1bdfe082051763ee2d1c4b54a5267
manuthomas99 <manuthomas233@gmail.com>
2025-11-16 12:08:25 +0100

    VCD code added

---
+++ Reefknot/LLaVA/vcd_patch_llava.py
@@ -0,0 +1,128 @@
+import argparse
+from Reefknot.LLaVA.infer_LLaVA_yesandno import get_chunk, get_path
+import torch
+import os
+import json
+from tqdm import tqdm
+import shortuuid
+import sys
+import os
+
+from llava.constants import IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN
+from llava.conversation import conv_templates, SeparatorStyle
+from llava.model.builder import load_pretrained_model
+from llava.utils import disable_torch_init
+from llava.mm_utils import tokenizer_image_token, get_model_name_from_path, KeywordsStoppingCriteria
+
+from PIL import Image
+import math
+
+# import kornia
+from transformers import set_seed
+from VCD.vcd_utils.vcd_add_noise import add_diffusion_noise
+from VCD.vcd_utils.vcd_sample import evolve_vcd_sampling
+evolve_vcd_sampling()
+
+
+def eval_model(args):
+    # Model
+    disable_torch_init()
+    model_path = os.path.expanduser(args.model_path)
+    model_name = get_model_name_from_path(model_path)
+    tokenizer, model, image_processor, context_len = load_pretrained_model(model_path, args.model_base, model_name,load_4bit=args.quantized, device_map="auto")
+    questions = [
+        json.loads(q) for q in open(os.path.expanduser(args.question_file), "r")
+    ]
+    questions = get_chunk(questions, args.num_chunks, args.chunk_idx)
+    answers_file = os.path.expanduser(args.answers_file)
+    os.makedirs(os.path.dirname(answers_file), exist_ok=True)
+    ans_file = open(answers_file, "w")
+    for line in tqdm(questions):
+        image_file = line["image_id"] + ".jpg"
+        # image_path = os.path.join(args.image_folder, image_file)
+        image_path = get_path(line["image_id"], args.image_folder)
+        if not os.path.exists(image_path):
+            print(f"Image file {image_file} not found, skipping.")
+            continue 
+
+        qs = line["query_prompt"]
+        label = line["label"]
+        cur_prompt = qs
+        if model.config.mm_use_im_start_end:
+            qs = DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_TOKEN + DEFAULT_IM_END_TOKEN + '\n' + qs
+        else:
+            qs = DEFAULT_IMAGE_TOKEN + '\n' + qs
+
+        conv = conv_templates[args.conv_mode].copy()
+        conv.append_message(conv.roles[0], qs + " Please answer this question with one word.")
+        conv.append_message(conv.roles[1], None)
+        prompt = conv.get_prompt()
+
+        input_ids = tokenizer_image_token(prompt, tokenizer, IMAGE_TOKEN_INDEX, return_tensors='pt').unsqueeze(0).to(model.device)
+
+        image = Image.open(os.path.join(args.image_folder, image_file))
+        image_tensor = image_processor.preprocess(image, return_tensors='pt')['pixel_values'][0]
+        
+        if args.use_cd:
+            image_tensor_cd = add_diffusion_noise(image_tensor, args.noise_step)
+        else:
+            image_tensor_cd = None      
+
+        # stop_str = conv.sep if conv.sep_style != SeparatorStyle.TWO else conv.sep2
+        # keywords = [stop_str]
+        # stopping_criteria = KeywordsStoppingCriteria(keywords, tokenizer, input_ids)
+
+        with torch.inference_mode():
+            output_ids = model.generate(
+                input_ids,
+                images=image_tensor.unsqueeze(0).half().to(model.device),
+                images_cd=(image_tensor_cd.unsqueeze(0).half().to(model.device) if image_tensor_cd is not None else None),
+                cd_alpha = args.cd_alpha,
+                cd_beta = args.cd_beta,
+                do_sample=True,
+                temperature=args.temperature,
+                max_new_tokens=args.max_new_tokens,
+                use_cache=True,
+                output_scores=True)
+
+        outputs = tokenizer.batch_decode(
+            output_ids, skip_special_tokens=True
+        )[0].strip()
+        mllm = args.model_path.split('/')[-1]
+        ans_file.write(
+            json.dumps(
+                {
+                    "image_id": line["image_id"],
+                    "query_prompt": cur_prompt,
+                    "response": outputs,
+                    "label": label,
+                    "mllm_name": mllm
+                }
+            )
+            + "\n"
+        )
+        ans_file.flush()
+    ans_file.close()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model-path", type=str, default="facebook/opt-350m")
+    parser.add_argument("--model-base", type=str, default=None)
+    parser.add_argument("--image-folder", type=str, default="")
+    parser.add_argument("--question-file", type=str, default="tables/question.jsonl")
+    parser.add_argument("--answers-file", type=str, default="answer.jsonl")
+    parser.add_argument("--conv-mode", type=str, default="llava_v1")
+    parser.add_argument("--num-chunks", type=int, default=1)
+    parser.add_argument("--chunk-idx", type=int, default=0)
+    parser.add_argument("--temperature", type=float, default=1.0)
+    parser.add_argument("--top_p", type=float, default=1)
+    parser.add_argument("--top_k", type=int, default=None)
+
+    parser.add_argument("--noise_step", type=int, default=500)
+    parser.add_argument("--use_cd", action='store_true', default=False)
+    parser.add_argument("--cd_alpha", type=float, default=1)
+    parser.add_argument("--cd_beta", type=float, default=0.1)
+    parser.add_argument("--seed", type=int, default=42)
+    args = parser.parse_args()
+    set_seed(args.seed)
+    eval_model(args)
