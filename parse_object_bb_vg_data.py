bf98617326cf377ff19cd02ce02f2fcf979319ed
manuthomas99 <manuthomas233@gmail.com>
2025-11-25 22:52:11 +0100

    script to parse object.json of VG Dataset to get BB

---
+++ parse_object_bb_vg_data.py
@@ -0,0 +1,39 @@
+"""
+Parse object bounding boxes from VG data
+expected input: objects.json from VG dataset
+output: json file with image_id as key and object bounding boxes as values
+"""
+
+import json
+import argparse
+from pathlib import Path
+
+def load_objects_json(path: str):
+    p = Path(path)
+    if not p.exists():
+        raise FileNotFoundError(f"{p} not found")
+    with p.open("r", encoding="utf-8") as fh:
+        return json.load(fh)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Parse object bounding boxes from VG data")
+    parser.add_argument("--input", type=str, required=True, help="Path to the input JSON file")
+    parser.add_argument("--output", type=str, required=True, help="Path to the output JSON file")
+    args = parser.parse_args()
+
+    objs = load_objects_json(args.input)
+    parsed_objects = { obj['image_id']: obj for obj in objs }
+    final_parsed = {}
+    for image in objs:
+        parsed_objects[image['image_id']] = {}
+        for obj in image['objects']:
+            if 'names' in obj and (len(obj['names']) > 1 or len(obj['names']) == 0):
+                print(f"Image ID {image['image_id']} has invalid object name: {obj['names']}")
+                import pdb; pdb.set_trace()                   
+            else:
+                parsed_objects[image['image_id']][obj['names'][0]] = {"x":obj["x"], "y":obj["y"], "w":obj["w"], "h":obj["h"]}   
+    
+    
+    with open( args.output, "w", encoding="utf-8") as fh:
+            json.dump(parsed_objects, fh, ensure_ascii=False, indent=4)
+    
