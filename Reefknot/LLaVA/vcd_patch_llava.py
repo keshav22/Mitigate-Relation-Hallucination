465411797205f991d75f8a482b93c05dbbd95b3a
manuthomas99 <manuthomas233@gmail.com>
2025-12-02 12:58:47 +0100

    correct import stmts for vcd seed

--- Reefknot/LLaVA/vcd_patch_llava.py
+++ Reefknot/LLaVA/vcd_patch_llava.py
@@ -7,13 +7,14 @@ from tqdm import tqdm
 import shortuuid
 import sys
 import os
-
+import random
+import numpy as np
 from llava.constants import IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN
 from llava.conversation import conv_templates, SeparatorStyle
 from llava.model.builder import load_pretrained_model
 from llava.utils import disable_torch_init
 from llava.mm_utils import tokenizer_image_token, get_model_name_from_path, KeywordsStoppingCriteria
-
+from transformers.trainer_utils import enable_full_determinism
 from PIL import Image
 import math
 
